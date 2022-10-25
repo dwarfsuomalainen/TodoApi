61c4abfef151c22820a6ab341dc3bc84c46d3d20
Roman Bogoudinov <r.bogoudinov@mac.com>
2022-10-25 22:09:27 +0300

    DataContext TodoContext

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,13 +1,12 @@
-using System.Threading;
-using System.Threading.Tasks;
+
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
  
 namespace TodoApi.Data
 {
-    public interface DataContext
+    public class DataContext: DbContext, TodoContext
     {
-        DbSet<Todo> Todos { get; init; }
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+        public DataContext(DbContextOptions<DataContext> options) : base(options);
     }
+    public DbSet<Todo> Todos {get; init;}
 }
--- Data/TodoContext.cs
+++ Data/TodoContext.cs
@@ -3,9 +3,11 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
+
+
 namespace TodoApi.Data
 {
-    public interface TodoContext: DbContext
+    public interface TodoContext
     {
         DbSet<Todo> Todos {get; init;}
 
