1997eaad40d18c92e9e04988c1ad5c2cac87bbb4
Roman Bogoudinov <r.bogoudinov@mac.com>
2022-10-25 23:04:10 +0300

    interface correction

--- Data/TodoContext.cs
+++ Data/TodoContext.cs
@@ -6,7 +6,7 @@ namespace TodoApi.Data
 {
     public class TodoContext: DbContext, ITodoContext
     {
-        public TodoContext(DbContextOptions<TodoContext> options): base(options);
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options);
     }
     public DbSet<Todo> Todos {get; init;}
 }
