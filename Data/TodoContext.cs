5b8556e609b198f82c805430744b9fdd7f65ce90
Roman Bogoudinov <r.bogoudinov@mac.com>
2022-10-25 21:52:50 +0300

    midnight commit

--- Data/TodoContext.cs
+++ Data/TodoContext.cs
@@ -1,12 +1,13 @@
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
 namespace TodoApi.Data
 {
-    public interface TodoContext
+    public interface TodoContext: DbContext
     {
-        DbSet<Todo> Todos {get; set;}
+        DbSet<Todo> Todos {get; init;}
 
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Data;
+using TodoApi.Models;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<TodoContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddScoped<TodoContext>(provider => provider.GetService<TodoContext>());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +27,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseAuthentication();
 
 app.MapControllers();
 
