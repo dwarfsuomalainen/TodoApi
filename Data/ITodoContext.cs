a95a146f9af69f88fc0ff63d06e8e44880d045a1
Roman Bogoudinov <r.bogoudinov@mac.com>
2022-10-26 08:38:47 +0300

    interfase issue

--- Data/ITodoContext.cs
+++ Data/ITodoContext.cs
@@ -3,14 +3,11 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 
-
-
 namespace TodoApi.Data
 {
     public interface ITodoContext
     {
-        DbSet<Todo> Todos {get; init;}
-
+        DbSet<Todo> Todos { get; init; }
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
